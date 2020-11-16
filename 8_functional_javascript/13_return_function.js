2484810b1a4fa90a12eeaa7ad455f64aaf0a8a74
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-16 12:09:20 -0800

    edited return function

---
+++ 8_functional_javascript/13_return_function.js
@@ -0,0 +1,32 @@
+function greet(msg) {
+  function greetings(name) {
+    return msg + ', ' + name + ' !';
+  }
+  return greetings;
+}
+
+// var gm = greet('Good Morning');
+// console.log(gm('Joney Talukdar'));
+// var gn = greet('Good Night');
+// var hello = greet('Hello');
+// console.log(gn('Joney'));
+// console.log(hello('Jerry'));
+
+function base(b) {
+  return function (n) {
+    var result = 1;
+    for (var i = 0; i < b; i++) {
+      result *= n;
+    }
+    return result;
+  };
+}
+
+var base10 = base(10);
+console.log(base10(2));
+
+var base5 = base(5);
+console.log(base5(2));
+console.log(base5(3));
+console.log(base5(4));
+console.log(base5(5));
