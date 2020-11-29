e46a48b7f6076fead8e76ab164edf97251bd64e3
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-29 11:23:58 -0800

    edited js engine

---
+++ 9_closure_&_scope/1_javascript_engine.js
@@ -0,0 +1,5 @@
+// javascript ingine - is an another program which can read understand and translate javascript code
+
+// javascript engine - v8 , spider-monkey , chakra , javascript core
+
+// sorce code  - parser - abstract window - machine code - execution
