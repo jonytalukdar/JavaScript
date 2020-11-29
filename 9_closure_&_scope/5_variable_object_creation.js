8d1db10e0444a86417ece47c19af5bd07ecee8fe
Jony Talukdar <jonytalukdar30@gmail.com>
2020-11-29 12:25:49 -0800

    edited varable creation execution context

---
+++ 9_closure_&_scope/5_variable_object_creation.js
@@ -0,0 +1,24 @@
+var a = 12;
+
+var b;
+
+console.log(a);
+console.log(b);
+
+b = 34;
+console.log(b);
+
+console.log(c);
+
+var c = 67;
+console.log(c);
+
+// creational phase
+// a = undefined
+// b = undefined
+// c = undefined
+
+//executional phase
+// a = 12
+// b = 34
+// c = 67
