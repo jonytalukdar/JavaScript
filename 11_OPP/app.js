44648f89d0b641b36556001e8e15e22e6884327d
Jony Talukdar <jonytalukdar30@gmail.com>
2020-12-13 12:25:27 -0800

    added some line

--- 11_OPP/app.js
+++ 11_OPP/app.js
@@ -119,29 +119,116 @@
 // var str = new String('this is a strings');
 // console.log('My strings is : ' + str);
 
-function test() {
-  console.log('something');
-}
+// function test() {
+//   console.log('something');
+// }
 
-test();
-console.log(test.name);
-console.log(test.length);
+// test();
+// console.log(test.name);
+// console.log(test.length);
 
-var Rect = Function(
-  'width',
-  'height',
-  `  this.width = width;
+// var Rect = Function(
+//   'width',
+//   'height',
+//   `  this.width = width;
+//   this.height = height;
+//   (this.draw = function () {
+//     console.log('this is rectangle');
+//     this.print();
+//   }),
+//     (this.print = function () {
+//       console.log('this is width :' + this.width);
+//       console.log('this is height : ' + this.height);
+//     });`
+// );
+
+// var rect5 = new Rect(12, 4);
+// rect5.draw();
+// console.log(typeof rect5);
+
+// function myFunc(c, d) {
+//   console.log(this);
+//   console.log(this.a + this.b + c + d);
+// }
+
+// call , apply
+//bind
+
+// myFunc.call({ a: 23, b: 12 }, 10, 5);
+// myFunc.apply({ a: 200, b: 100 }, [12, 12]);
+// myFunc();
+
+// var test = myFunc.bind({ a: 12, b: 12 }, 12, 12);
+// test();
+
+// pass by value vs pass by reference
+// pass by call vs pass by reference
+
+// var n = 12;
+
+// function change(n) {
+//   n = n + 100;
+//   console.log(n);
+// }
+// change(n);
+// console.log(n);
+
+// var obj = {
+//   a: 23,
+//   b: 34,
+// };
+
+// function changeObject(obj) {
+//   obj.a = obj.a + 100;
+//   obj.b = obj.b + 100;
+//   console.log(obj);
+// }
+
+// changeObject(obj);
+
+// console.log(obj);
+
+var Reactangle = function (width, height) {
+  // var name = 'joney'
+
+  this.width = width;
   this.height = height;
-  (this.draw = function () {
-    console.log('this is rectangle');
-    this.print();
-  }),
-    (this.print = function () {
-      console.log('this is width :' + this.width);
-      console.log('this is height : ' + this.height);
-    });`
-);
+  var position = {
+    x: 56,
+    y: -100,
+  };
 
-var rect5 = new Rect(12, 4);
-rect5.draw();
-console.log(typeof rect5);
+  var print = function () {
+    console.log('this is width :' + this.width);
+    console.log('this is height : ' + this.height);
+  }.bind(this);
+
+  // this.getPosition = function () {
+  //   return position;
+  // };
+
+  this.draw = function () {
+    console.log('this is rectangle');
+    print();
+    console.log('Position x : ' + position.x, 'position y : ' + position.y);
+  };
+
+  Object.defineProperty(this, 'position', {
+    get: function () {
+      return position;
+    },
+    set: function (value) {
+      position = value;
+    },
+  });
+};
+
+var rect7 = new Reactangle(12, 3);
+rect7.draw();
+// rect7.getPosition();
+// console.log(rect7.getPosition());
+rect7.position = {
+  x: 234,
+  y: 567,
+};
+console.log(rect7.position);
