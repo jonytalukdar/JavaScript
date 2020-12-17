c49b95233885021b60f1e234e8976d883c732d5e
Jony Talukdar <jonytalukdar30@gmail.com>
2020-12-16 22:26:13 -0800

    added some line in arrow function syntax

---
+++ 14_ES6/arrow_function_syntax.js
@@ -0,0 +1,23 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// let result = add(12, 12);
+// console.log(result);
+
+let add = (a, b) => a + b;
+let result = add(12, 12);
+console.log(result);
+
+let sqr = (x) => x * x;
+let result1 = sqr(5);
+console.log(result1);
+
+// function add(a, b) {
+//   console.log(a + b);
+// }
+// add(12, 45);
+
+// let add = (a, b) => a + b;
+// let result2 = add(12, 45);
+// console.log(result2);
