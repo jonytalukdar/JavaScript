4aa37db0c2168b0f91c998a8cba535ea344c392c
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-10 20:01:59 +0600

    added some line in storeResult.js file

---
+++ 17_asynchronous/storeResult.js
@@ -0,0 +1,14 @@
+function sayMyName(name) {
+  //   let result;
+  setTimeout(() => {
+    result = name;
+    // console.log('i have done');
+    console.log(name);
+  }, 3000);
+  //   return result;
+}
+
+// let output = sayMyName('Joney Talukdar');
+// console.log(output);
+
+sayMyName('Joney Talukdar');
