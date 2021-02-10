0c4cc6f54dd9fea1ae6bbb2f9ed8398825d6f6a6
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-10 19:35:41 +0600

    added some code in intro.js

---
+++ 17_asynchronous/intro.js
@@ -0,0 +1,12 @@
+var a = 12;
+var b = 34;
+var result = a + b;
+console.log(result);
+
+function storeSome(result)
+
+if(Response.succes){
+    console.log('succes')
+}else{
+    console.log('unsuccess')
+}
