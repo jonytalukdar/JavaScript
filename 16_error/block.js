65f21b3b8258bb37b124724fbfa37fa71f99eacc
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-10 19:11:57 +0600

    added some line in

---
+++ 16_error/block.js
@@ -0,0 +1,11 @@
+try {
+  console.log('I am line one');
+  console.log('I am line two');
+  throw new Error('This is your Error');
+  console.log('I am line three');
+  console.log('I am line four');
+} catch (error) {
+  console.log(error.mesasge);
+} finally {
+  console.log('I am finaly Block');
+}
