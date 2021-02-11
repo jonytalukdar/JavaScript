4efc9ad1828fe8487bfb24be03cc2ead1bbeab08
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-11 13:51:13 +0600

    created ad new file called jsHandle.js

---
+++ 17_asynchronous/why.js
@@ -0,0 +1,12 @@
+let responseA = serverCall(url);
+let responseB = serverCall(url);
+let responseC = serverCall(url);
+
+console.log(responseA);
+console.log(responseB);
+console.log(responseC);
+
+// call Stack
+// web api
+// event loop
+// callBack queue
